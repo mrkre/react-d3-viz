a4dd9b59fb2b1436b0383d0ab74be0059ee436c3
yaohong <yaohong.chng@gmail.com>
2017-05-07 20:25:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# react-d3-viz
+
+Create visualizations in React with D3.
+
+## Installation
+
+Install with npm using:
+
+```
+npm install --save react-d3-viz
+```
+
+Or yarn:
+
+```
+yarn add react-d3-viz
+```
+
---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import babel from 'rollup-plugin-babel';
+import babelrc from 'babelrc-rollup';
+import istanbul from 'rollup-plugin-istanbul';
+import localResolve from 'rollup-plugin-local-resolve';
+
+const pkg = require('./package.json');
+
+export default {
+  entry: 'lib/index.js',
+  dest: 'index.js',
+  format: 'cjs',
+  plugins: [
+    babel(babelrc({ addModuleOptions: false })),
+    localResolve(),
+    istanbul({
+      exclude: ['tests/**/*', 'node_modules/**/*'],
+    }),
+  ],
+  external: ['react', 'react-dom'],
+  globals: {
+    react: 'React',
+  },
+  targets: [
+    {
+      dest: pkg.main,
+      format: 'umd',
+      moduleName: 'rollupStarterProject',
+      sourceMap: true,
+    },
+    {
+      dest: pkg.module,
+      format: 'es',
+      sourceMap: true,
+    },
+  ],
+};
